dc38b65876326f6ff3befb0924e008ea1757be3b
linwudicoder <2418576481@qq.com>
2021-04-13 13:40:53 +0800

    day04-1

--- src/main/java/team/tb/controller/IndexController.java
+++ src/main/java/team/tb/controller/IndexController.java
@@ -1,22 +1,56 @@
 package team.tb.controller;
 
+import cn.dsna.util.images.ValidateCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
- * 中转静态页面
+ * 中转静态页面，保证经过springmvc的模板视图解析器
  */
 @Controller
 @RequestMapping("/pages")
 public class IndexController {
 
-    @RequestMapping("/adminPanel.html")
+    @RequestMapping("/admin/adminPanel.html")
     public String adminPanel(){
-        return "adminPanel";
+        return "admin/adminPanel";
     }
 
     @RequestMapping("/index.html")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/user/userPanel.html")
+    public String userPanel(){
+        return "user/userPanel";
+    }
+
+    @RequestMapping("/root/rootPanel.html")
+    public String rootPanel(){
+        return "root/rootPanel";
+    }
+
+    /**
+     * 获取验证码
+     * @param request
+     * @param response
+     */
+    @RequestMapping("/getCode")
+    public void getCode(HttpServletRequest request, HttpServletResponse response){
+        //生成验证码对象, lineCount是干扰条数
+        ValidateCode validateCode = new ValidateCode(120, 40, 4, 50);
+        //在后台设置session存放正确的验证码
+        request.getSession().setAttribute("code", validateCode.getCode());
+        try{
+            //将验证码图片以二进制方式返回给前端
+            validateCode.write(response.getOutputStream());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/team/tb/controller/UserController.java
+++ src/main/java/team/tb/controller/UserController.java
@@ -3,6 +3,7 @@ package team.tb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import team.tb.common.Result;
 import team.tb.pojo.User;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/user")
@@ -29,37 +31,60 @@ public class UserController extends BaseController{
         return "login";
     }
 
-    @RequestMapping("/login")
+    /**
+     * 用户登录，传入用户名和密码，验证码
+     * 请求方式：POST
+     * @param user
+     * @param code
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public Result login(User user, HttpServletRequest request, HttpServletResponse response){
-        User ret = null;
-        if(!StringUtils.isEmpty(user.getUsername()) && !StringUtils.isEmpty(user.getPassword())){
-            // 将传递进来的密码进行加密
-            user.setPassword(MD5Utils.encryption(user.getUsername(), user.getPassword()));
-            ret = userService.findUserByUsernameAndPwd(user);
+    public Result login(User user, String code, HttpServletRequest request, HttpServletResponse response){
+        // 拿出在session中的验证码
+        String codeInSession = (String) request.getSession().getAttribute("code");
+        // 验证码相等
+        if(!StringUtils.isEmpty(code) && code.equalsIgnoreCase(codeInSession)){
+            User ret = null;
+            if(!StringUtils.isEmpty(user.getUsername()) && !StringUtils.isEmpty(user.getPassword())){
+                // 将传递进来的密码进行加密
+                user.setPassword(MD5Utils.encryption(user.getUsername(), user.getPassword()));
+                ret = userService.findUserByUsernameAndPwd(user);
+            }
+            if(ret != null){
+                // 将对象存入session中
+                request.getSession().setAttribute("user", ret);
+                // 将用户id，用户名生成token
+                Map<String, String> map = new HashMap<>();
+                map.put("id", String.valueOf(ret.getUid()));
+                map.put("username", ret.getUsername());
+                // 生成token
+                String token = JWTUtils.getToken(map);
+                Cookie cookie = new Cookie("token", token);
+                // 设置cookie存活时间为一周
+                cookie.setMaxAge(60*60*24*7);
+                response.addCookie(cookie);
+                // 依据登陆者身份跳转页面
+                String path = "pages";
+                if(ret.getUlevel() == 0){
+                    path += "/user/userPanel.html";
+                }else if(ret.getUlevel() == 1){
+                    path += "/admin/adminPanel.html";
+                }else{
+                    path += "/root/rootPanel.html";
+                }
+                return Result.succ("登陆成功 " + path);
+            }
+            return Result.fail("账号或密码错误");
         }
-        if(ret != null){
-            // 将对象存入session中
-            request.getSession().setAttribute("user", ret);
-            Map<String, String> map = new HashMap<>();
-            map.put("id", String.valueOf(ret.getUid()));
-            map.put("username", ret.getUsername());
-            // 生成token
-            String token = JWTUtils.getToken(map);
-            Cookie cookie = new Cookie("token", token);
-            // 设置cookie存活时间为一周
-            cookie.setMaxAge(60*60*24*7);
-            response.addCookie(cookie);
-//            return "system/index";
-            return Result.succ("登陆成功");
-        }
-//        return "error/error";
-        return Result.fail("登陆失败");
+        return Result.fail(401, "验证码错误", null);
     }
 
     @RequestMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response){
-        // 移除session中的信息，将cookie的信息也清除
+        // 移除session中的信息，将cookie的认证信息清除
         request.getSession().invalidate();
         Cookie cookie = new Cookie("token", "");
         cookie.setMaxAge(0);
